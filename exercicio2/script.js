d0fd94f4e17b364dd01173b3438a3ed69d2b4dd3
vargasbernardo <vargasbernardo97@gmail.com>
2023-01-31 14:44:27 -0300

    Exercicios 1, 2 e 3 feitos

---
+++ exercicio2/script.js
@@ -0,0 +1,17 @@
+// Exercicio 2 
+
+// let a = 25
+// let b = 15
+// let c 
+
+// c valor de a
+// c = a
+// console.log('O valor de c eh:', a)
+
+// a valor de b 
+// a = b 
+// console.log('o valor de a eh:', b)
+
+// b valor de c 
+// b = c
+// console.log('o valor de b eh:', c)
---
+++ exercicio3/script.js
@@ -0,0 +1,51 @@
+// Exercicio 3
+
+const nomeCompleto = 'Qual seu nome completo?'
+const respostaNomeCompleto = prompt(nomeCompleto)
+console.log(respostaNomeCompleto)
+console.log(typeof respostaNomeCompleto)
+
+const dataDeNascimento = 'Qual a sua data de nascimento?'
+const respostaDataDeNascimento = prompt(dataDeNascimento)
+console.log(respostaDataDeNascimento)
+console.log(typeof respostaDataDeNascimento)
+
+let endereco = 'Informe seu endereço'
+let respostaEndereco = prompt(endereco)
+console.log(respostaEndereco)
+console.log(typeof respostaEndereco)
+
+const documentoCpf = 'Informe seu número de CPF'
+const respostaDocumentoCpf = prompt(documentoCpf)
+console.log(respostaDocumentoCpf)
+console.log(typeof respostaDocumentoCpf)
+
+let nivelDeEscolaridade = 'Qual seu nivel de escolaridade?'
+let respostaNivelDeEscolaridade = prompt(nivelDeEscolaridade)
+console.log(respostaNivelDeEscolaridade)
+console.log(typeof respostaNivelDeEscolaridade)
+
+let possuiCnh = 'Possui CNH?'
+let respostaPossuiCnh = prompt(possuiCnh)
+console.log(respostaPossuiCnh)
+console.log(typeof respostaPossuiCnh)
+
+let numeroDeFilhes = 'Quantos filhes possui?'
+let respostaNumeroDeFilhes = Number(prompt(numeroDeFilhes))
+console.log(respostaNumeroDeFilhes)
+console.log(typeof respostaNumeroDeFilhes)
+
+let cargoAtual = 'Qual o seu cargo atual?'
+let respostaCargoAtual = prompt(cargoAtual)
+console.log(respostaCargoAtual)
+console.log(typeof respostaCargoAtual)
+
+let recebeComissao = 'Recebe comissao? Se sim informe a porcentagem, caso contrario, responda com zero'
+let respostaRecebeComissao = Number(prompt(recebeComissao))
+console.log(respostaRecebeComissao)
+console.log(typeof respostaRecebeComissao)
+
+const anoDeAdmissao = 'Qual o ano de admissao?'
+const respostaAnoDeAdmissao = Number(prompt(anoDeAdmissao))
+console.log(respostaAnoDeAdmissao)
+console.log(typeof respostaAnoDeAdmissao)
