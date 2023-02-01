70d9502d8cd5d9f82fc6eb7e294078a10696dac1
vargasbernardo <vargasbernardo97@gmail.com>
2023-01-31 22:34:33 -0300

    exercicio 3 mais organizado

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,51 +1,38 @@
 // Exercicio 3
 
+// variaveis perguntas e respostas
 const nomeCompleto = 'Qual seu nome completo?'
 const respostaNomeCompleto = prompt(nomeCompleto)
-console.log(respostaNomeCompleto)
-console.log(typeof respostaNomeCompleto)
-
 const dataDeNascimento = 'Qual a sua data de nascimento?'
 const respostaDataDeNascimento = prompt(dataDeNascimento)
-console.log(respostaDataDeNascimento)
-console.log(typeof respostaDataDeNascimento)
-
 let endereco = 'Informe seu endereço'
 let respostaEndereco = prompt(endereco)
-console.log(respostaEndereco)
-console.log(typeof respostaEndereco)
-
 const documentoCpf = 'Informe seu número de CPF'
 const respostaDocumentoCpf = prompt(documentoCpf)
-console.log(respostaDocumentoCpf)
-console.log(typeof respostaDocumentoCpf)
-
 let nivelDeEscolaridade = 'Qual seu nivel de escolaridade?'
 let respostaNivelDeEscolaridade = prompt(nivelDeEscolaridade)
-console.log(respostaNivelDeEscolaridade)
-console.log(typeof respostaNivelDeEscolaridade)
-
 let possuiCnh = 'Possui CNH?'
-let respostaPossuiCnh = prompt(possuiCnh)
-console.log(respostaPossuiCnh)
-console.log(typeof respostaPossuiCnh)
-
+let respostaPossuiCnh = confirm(possuiCnh)
 let numeroDeFilhes = 'Quantos filhes possui?'
 let respostaNumeroDeFilhes = Number(prompt(numeroDeFilhes))
-console.log(respostaNumeroDeFilhes)
-console.log(typeof respostaNumeroDeFilhes)
-
 let cargoAtual = 'Qual o seu cargo atual?'
 let respostaCargoAtual = prompt(cargoAtual)
-console.log(respostaCargoAtual)
-console.log(typeof respostaCargoAtual)
-
 let recebeComissao = 'Recebe comissao? Se sim informe a porcentagem, caso contrario, responda com zero'
 let respostaRecebeComissao = Number(prompt(recebeComissao))
-console.log(respostaRecebeComissao)
-console.log(typeof respostaRecebeComissao)
-
 const anoDeAdmissao = 'Qual o ano de admissao?'
 const respostaAnoDeAdmissao = Number(prompt(anoDeAdmissao))
-console.log(respostaAnoDeAdmissao)
-console.log(typeof respostaAnoDeAdmissao)
+
+// impressao typeof e relatorio pessoa colaboradora
+console.log(`
+${nomeCompleto} - ${respostaNomeCompleto}
+${dataDeNascimento} - ${respostaDataDeNascimento}
+${endereco} - ${respostaEndereco}
+${documentoCpf} - ${respostaDocumentoCpf}
+${nivelDeEscolaridade} - ${respostaNivelDeEscolaridade}
+${possuiCnh} - ${respostaPossuiCnh}
+${numeroDeFilhes} - ${respostaNumeroDeFilhes}
+${cargoAtual} - ${respostaCargoAtual}
+${recebeComissao} - ${respostaRecebeComissao}
+${anoDeAdmissao} - ${respostaAnoDeAdmissao} `)
+
+console.log(typeof respostaNomeCompleto, typeof respostaDataDeNascimento, typeof respostaEndereco, typeof respostaDocumentoCpf, typeof respostaNivelDeEscolaridade, typeof respostaPossuiCnh, typeof respostaNumeroDeFilhes, typeof respostaCargoAtual, typeof respostaRecebeComissao, typeof respostaAnoDeAdmissao )
